e74ee20972638ee2b2d0fcf7196a73bf16244796
doker123 <dodo2larin@gmail.com>
2025-11-12 10:39:18 +0700

    Добавил в зависимости django rest framework

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import QuestionViewSet, vote_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = "api"
 
@@ -12,3 +14,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('questions/<int:pk>/vote/', vote_view, name='vote-api'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
